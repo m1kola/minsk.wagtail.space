108d49d95cd926368b2256ea492da833670ce52a
Mikalai Radchuk <radchuk.m@gmail.com>
2018-09-22 22:44:53 +0100

    Fixes the UFO appearance effect

--- src/components/ufo.js
+++ src/components/ufo.js
@@ -3,14 +3,21 @@ import PropTypes from 'prop-types'
 
 import wagtailUfo from '../assets/img/wagtail-ufo.svg'
 
-const UFO = (props) => (
-    <div className="ufo" style={{
-        opacity: props.top > 0 || props.left > 0 ? 1 : 0,
-        transform: `translate(${props.top}px, ${props.left}px)`
-    }}>
-        <img src={wagtailUfo} alt="UFO" />
-    </div>
-)
+const UFO = (props) => {
+    let style = {}
+    if (props.top > 0 || props.left > 0) {
+        style = {
+            opacity: 1,
+            transform: `translate(${props.top}px, ${props.left}px)`
+        }
+    }
+
+    return (
+        <div className="ufo" style={style}>
+            <img src={wagtailUfo} alt="UFO" />
+        </div>
+    )
+}
 
 UFO.propTypes = {
     top: PropTypes.number,
