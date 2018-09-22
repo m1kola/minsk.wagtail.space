9a2ee94e57a5af762db0d0afdee6a7ba1f377cf9
Mikalai Radchuk <radchuk.m@gmail.com>
2018-09-22 19:24:23 +0100

    Makes the "Read more" work with smooth scroll

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,47 +1,60 @@
 import React from 'react'
+import { Element, scroller } from 'react-scroll'
 import Helmet from 'react-helmet'
-import { Link, graphql} from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-
 class BlogPostTemplate extends React.Component {
+  scrollToMainContent() {
+    scroller.scrollTo('main-content', {
+      duration: 800,
+      delay: 0,
+      smooth: 'easeInOutQuart'
+    })
+  }
+
   render() {
     const post = this.props.data.markdownRemark
-    const { previous, next } = this.props.pageContext
 
     const meta = []
     if (post.frontmatter.title) {
-      meta.push({property: 'og:title', content: post.frontmatter.title})
-      meta.push({name: 'twitter:title', content: post.frontmatter.title})
+      meta.push({ property: 'og:title', content: post.frontmatter.title })
+      meta.push({ name: 'twitter:title', content: post.frontmatter.title })
     }
     if (post.frontmatter.description) {
-      meta.push({name: 'description', content: post.frontmatter.description})
-      meta.push({name: 'twitter:description', content: post.frontmatter.description})
-      meta.push({property: 'og:description', content: post.frontmatter.description})
+      meta.push({ name: 'description', content: post.frontmatter.description })
+      meta.push({ name: 'twitter:description', content: post.frontmatter.description })
+      meta.push({ property: 'og:description', content: post.frontmatter.description })
     }
     if (post.frontmatter.shareImage) {
       const imageURL = this.props.data.site.siteMetadata.siteUrl + post.frontmatter.shareImage.childImageSharp.resize.src;
-      meta.push({name: 'twitter:image', content: imageURL})
-      meta.push({property: 'og:image', content: imageURL})
-      meta.push({property: 'og:image:width', content: post.frontmatter.shareImage.childImageSharp.resize.width})
-      meta.push({property: 'og:image:height', content: post.frontmatter.shareImage.childImageSharp.resize.height})
+      meta.push({ name: 'twitter:image', content: imageURL })
+      meta.push({ property: 'og:image', content: imageURL })
+      meta.push({ property: 'og:image:width', content: post.frontmatter.shareImage.childImageSharp.resize.width })
+      meta.push({ property: 'og:image:height', content: post.frontmatter.shareImage.childImageSharp.resize.height })
     }
 
     return (
       <Layout location={this.props.location}>
-        <Header title={post.frontmatter.title} subtitle={post.frontmatter.subtitle} />
         <Helmet title={post.frontmatter.title} meta={meta} />
+        <Header
+          title={post.frontmatter.title}
+          subtitle={post.frontmatter.subtitle}
+          readMoreOnClick={() => this.scrollToMainContent()}
+        />
 
-        <section className="chapter">
-          <div className="row">
-            <div className="col-md-10 offset-md-1">
-              <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <Element name="main-content">
+          <section ref={this.mainContentRef} className="chapter">
+            <div className="row">
+              <div className="col-md-10 offset-md-1">
+                <div dangerouslySetInnerHTML={{ __html: post.html }} />
+              </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </Element>
 
         <Footer />
       </Layout>
